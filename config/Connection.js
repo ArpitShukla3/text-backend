db1edcc3c141ad1ffd008b1cb7f7f1e9fa985419
ArpitShukla3 <arpit900shukla@gmail.com>
2024-01-03 16:42:27 +0530

    fixed mongodb connection issue

--- config/Connection.js
+++ config/Connection.js
@@ -8,7 +8,7 @@ const options = {
 async function dataBaseConnect() {
     const Url = process.env.Url;
     try {
-        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2")
+        const conn = await mongoose.connect(Url)
         console.log("connected to :", conn.connection.host);
     } catch (error) {
         console.log("Error occurred due to  " + error);
